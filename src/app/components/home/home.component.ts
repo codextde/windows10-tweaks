bfc2b00b114204456765644e9cd7af047dcd64a8
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2020-11-26 18:10:50 +0100

    Added Github Action

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,44 +5,14 @@ declare var navigator: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   constructor(
     private navCtrl: NavController,
     private electron: ElectronService
   ) {}
-  ngOnInit() {
-    /*this.electron.desktopCapturer
-      .getSources({ types: ['window', 'screen'] })
-      .then(async sources => {
-        for (const source of sources) {
-          console.log(source);
-          if (source.name == 'Screen 1') {
-            try {
-              const stream = await navigator.mediaDevices.getUserMedia({
-                audio: false,
-                video: {
-                  mandatory: {
-                    chromeMediaSource: 'desktop'
-                  }
-                }
-              });
-              this.handleStream(stream);
-            } catch (e) {
-              console.log(e);
-            }
-            return;
-          }
-        }
-      });*/
-  }
-
-  handleStream(stream) {
-    const video = document.querySelector('video');
-    video.srcObject = stream;
-    video.onloadedmetadata = e => video.play();
-  }
+  ngOnInit() {}
 
   goTo(url) {
     this.navCtrl.navigateRoot(url);

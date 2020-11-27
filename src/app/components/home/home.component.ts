62bada04e46be134a651a5b97f946da0ce82a415
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2020-11-27 18:53:58 +0100

    Added new Script

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,18 +1,59 @@
-import { ElectronService } from './../../services/electron.service';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
-declare var navigator: any;
+import { Plugins } from '@capacitor/core';
+import { NavController, ToastController } from '@ionic/angular';
+import { ClearRecentlyAccessdFilesScript } from './../../../@codext/scripts/1_clear-recently-accessd-files.script';
+import { EndScript } from './../../../@codext/scripts/end.script';
+import { InitialScript } from './../../../@codext/scripts/initial.script';
+import { CommandService } from './../../services/command.service';
+
+const { Filesystem } = Plugins;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  scripts: any;
   constructor(
     private navCtrl: NavController,
-    private electron: ElectronService
+    private command: CommandService,
+    private toastCtrl: ToastController,
+    private http: HttpClient
   ) {}
-  ngOnInit() {}
+  async ngOnInit() {
+    await this.loadScripts();
+  }
+
+  async loadScripts() {
+    this.scripts = await this.http
+      .get('http://localhost:1337/scripts')
+      .toPromise();
+  }
+
+  async runRemoteScript(script) {
+    script.loading = true;
+    console.log(script.script);
+
+    try {
+      const fullScript =
+        InitialScript.script + script.script + EndScript.script;
+      await this.command.batch(fullScript);
+      const toast = await this.toastCtrl.create({
+        message: 'Auftrag erfolgreich ausgeführt',
+        duration: 4000,
+      });
+      toast.present();
+    } catch (error) {
+      const toast = await this.toastCtrl.create({
+        message: 'Fehler beim ausführen des Auftrags',
+        duration: 4000,
+      });
+      toast.present();
+    } finally {
+      script.loading = false;
+    }
+  }
 
   goTo(url) {
     this.navCtrl.navigateRoot(url);
